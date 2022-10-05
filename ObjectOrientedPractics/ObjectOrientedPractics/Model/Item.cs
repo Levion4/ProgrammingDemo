4a9ec625c4f98b745d208c517b98391cc1d558d0
Лев Лунев <levlunev91@gmail.com>
2022-10-05 21:26:21 +0700

    Лабораторная работа 1: добавлены классы Item и ValueValidator, добавлены xml-комментарии для этих классов.

---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Model/Item.cs
@@ -0,0 +1,157 @@
+﻿using ObjectOrientedPractics.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Хранит данные о товаре.
+    /// </summary>
+    public class Item
+    {
+        /// <summary>
+        /// Ограничение на количество символов в названии товара.
+        /// </summary>
+        private readonly int _maxLengthName = 200;
+
+        /// <summary>
+        /// Ограничение на количество символов в описании товара.
+        /// </summary>
+        private readonly int _maxLengthInfo = 1000;
+
+        /// <summary>
+        /// Ограничение на минимальную цену товара.
+        /// </summary>
+        private readonly double _minCost = 0;
+
+        /// <summary>
+        /// Ограничение на максимальную цену товара.
+        /// </summary>
+        private readonly double _maxCost = 100000;
+
+        /// <summary>
+        /// Счетчик всех существующих объектов товаров.
+        /// </summary>
+        private static int _allItemsCount;
+
+        /// <summary>
+        /// Уникальный идентификатор для всех объектов данного класса.
+        /// </summary>
+        private int _id;
+
+        /// <summary>
+        /// Название товара.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Описание товара.
+        /// </summary>
+        private string _info;
+
+        /// <summary>
+        /// Стоимость товара.
+        /// </summary>
+        private double _cost;
+
+        /// <summary>
+        /// Возвращает и задает название товара.
+        /// Длина не более 200 символов.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value,
+                    _maxLengthName, nameof(Name));
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает описание товара.
+        /// Длина не более 1000 символов.
+        /// </summary>
+        public string Info
+        {
+            get
+            {
+                return _info;
+            }
+            set
+            {
+                ValueValidator.AssertStringOnLength(value,
+                    _maxLengthInfo, nameof(Info));
+                _info = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает стоимость товара.
+        /// Стоимость должна быть не меньше 0 и не больше 100000.
+        /// </summary>
+        public double Cost
+        {
+            get
+            {
+                return _cost;
+            }
+            set
+            {
+                ValueValidator.AssertValueInRange(value,
+                    _minCost, _maxCost, nameof(Cost));
+                _cost = value;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает уникальный идентификатор товара.
+        /// Задает только во время инициализации.
+        /// </summary>
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            private set
+            {
+                _id = value;
+            }
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Item"/>
+        /// </summary>
+        public Item()
+        {
+            _allItemsCount++;
+            _id = _allItemsCount;
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Item"/>
+        /// </summary>
+        /// <param name="name">Название.
+        /// Должно быть не длинее 200 символов.</param>
+        /// <param name="info">Описание.
+        /// Должно быть не длинее 1000 символов.</param>
+        /// <param name="cost">Стоимость. Должна быть
+        /// не меньше 0 и не больше 100000.</param>
+        public Item(string name, string info, double cost)
+        {
+            Name = name;
+            Info = info;
+            Cost = cost;
+            _allItemsCount++;
+            _id = _allItemsCount;
+        }
+    }
+}
---
+++ ObjectOrientedPractics/ObjectOrientedPractics/Services/ValueValidator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Services
+{
+    /// <summary>
+    /// Предоставляет методы для валидации.
+    /// </summary>
+    public static class ValueValidator
+    {
+        /// <summary>
+        /// Проверяет строку на длину.
+        /// </summary>
+        /// <param name="value">Проверяемая строка.</param>
+        /// <param name="maxLength">Максимально возможная длина.</param>
+        /// <param name="propertyName">Имя свойства или объекта, которое
+        /// подлежит проверке.</param>
+        /// <exception cref="ArgumentException">Возникает, когда
+        /// длина строки больше максимально возможной длины.</exception>
+        public static void AssertStringOnLength(string value,
+            int maxLength, string propertyName)
+        {
+            if (value.Length > maxLength)
+            {
+                throw new ArgumentException(
+                    $"The {propertyName} should be in the range" +
+                    $" up to {maxLength}, but was {value}.");
+            }
+        }
+
+        /// <summary>
+        /// Проверяет, что значение входит в заданный диапазон.
+        /// </summary>
+        /// <param name="value">Проверяемое значение.</param>
+        /// <param name="min">Нижняя граница диапазона.</param>
+        /// <param name="max">Верхняя граница диапазона.</param>
+        /// <param name="propertyName">Имя свойства или объекта, которое
+        /// подлежит проверке.</param>
+        /// <exception cref="ArgumentException">Возникает, 
+        /// когда значение не входит в заданный диапазон.</exception>
+        public static void AssertValueInRange(double value, double min, double max, string propertyName)
+        {
+            if (value < min || value > max)
+            {
+                throw new ArgumentException(
+                    $"The {propertyName} should be in the range from {min} to {max}, " +
+                    $"but was {value}");
+            }
+        }
+    }
+}
